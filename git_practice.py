eae7ddea17debe7096410c11571597e65fb176ec
shearr-csp <shearr@csp.edu>
2025-10-09 23:02:16 -0400

    Update git_practice.py
    Added date and favorite concept.

--- git_practice.py
+++ git_practice.py
@@ -1,2 +1,5 @@
 # get_practice.py
 print("Hello from Randy Shear!")
+import datetime
+print(f"Today's date is: {datetime.date.today()}")
+print("My favorite programming concept is conditional statements.")
